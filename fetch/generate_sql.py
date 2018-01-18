d71d9dbbf7df0e258fb463f7fefc7617d925256e
kezian <kezian@umich.edu>
2018-01-18 11:38:53 -0500

    finish building database

--- fetch/generate_sql.py
+++ fetch/generate_sql.py
@@ -2,18 +2,30 @@ import os
 import re
 import flask
 
-departments = ['EECS', 'STATS']
+departments_file = open('data/departments.txt')
+departments = departments_file.readlines()
 sql = ""
 
 department_id = 0
+sql = "INSERT INTO departments (departmentid, name) VALUES \n"
 for department in departments:
+    department = department[:-1]
     department_id += 1
-    if os.path.exists(os.path.join('data',department+'_professors.txt')):
-        read_file = open(os.path.join('data',department+'_professors.txt'))
+    sql += "(" + str(department_id) + ", '" + department + "'), \n"
+
+sql = sql[:-3] + ";\n"
+
+department_id = 0
+for department in departments:
+    department = department[:-1]
+    department_id += 1
+    if os.path.exists(os.path.join('data', department + '_professors.txt')):
+        read_file = open(os.path.join('data', department + '_professors.txt'))
         professors = list(set(read_file.readlines()))
         sql += "\nINSERT INTO professors (departmentid, name) VALUES \n"
         for professor in professors:
-            sql += "(" + str(department_id) + ", '" + flask.Markup(professor[:-1]).unescape() + "'), \n"
+            sql += "(" + str(department_id) + ", '" + flask.Markup(
+                professor[:-1]).unescape().replace("'", "''") + "'), \n"
         sql = sql[:-3] + ";\n"
     if os.path.exists(os.path.join('data', department + '_courses.txt')):
         read_file = open(os.path.join('data', department + '_courses.txt'))
@@ -22,7 +34,7 @@ for department in departments:
         for course in courses:
             result = re.findall('^\w+ (\d{3}): (.+)$', course)[0]
             number = result[0]
-            title = flask.Markup(result[1]).unescape()
+            title = flask.Markup(result[1]).unescape().replace("'", "''")
             sql += "(" + str(department_id) + ", " + number + ", '" + title + "'), \n"
         sql = sql[:-3] + ";\n"
 
