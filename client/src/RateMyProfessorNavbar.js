48e5c0a550fc6bc134ff01133b2d550a3dce7256
kezian <kezian@umich.edu>
2018-03-06 02:41:01 -0500

    finish non-collapse navbar

--- client/src/RateMyProfessorNavbar.js
+++ client/src/RateMyProfessorNavbar.js
@@ -1,7 +1,25 @@
 import React from 'react';
-import { Menu, Icon } from 'antd';
+import {Menu, Icon} from 'antd';
+import logoM from "./images/logo-M.png";
+import logoCSSA from "./images/logo-CSSA.png";
+
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
+
+const activeStyle = {
+    backgroundColor: '#1890ff',
+    color: '#ffffff'
+};
+
+const centerParentStyle = {
+    display: 'table'
+};
+
+const centerChildStyle = {
+    display: 'table-cell',
+    verticalAlign: 'middle',
+    textAlign: 'center',
+};
 
 export default class RateMyProfessorNavbar extends React.Component {
     state = {
@@ -14,6 +32,7 @@ export default class RateMyProfessorNavbar extends React.Component {
         });
     }
 
+
     render() {
         return (
             <Menu
@@ -21,26 +40,66 @@ export default class RateMyProfessorNavbar extends React.Component {
                 onClick={this.handleClick}
                 selectedKeys={[this.state.current]}
                 mode="horizontal"
+                style={{position:'fixed',width:'100%', top:0, left:0}}
             >
-                <Menu.Item key="mail">
-                    <Icon type="mail" />Navigation One
+                <Menu.Item key="logoM" style={{height: 46, width: 70, marginLeft: 20}}>
+                    <a href="/">
+                        <img src={logoM}
+                             style={{
+                                 height: 40,
+                                 position: 'absolute',
+                                 margin: 'auto',
+                                 top: 0,
+                                 left: 0,
+                                 right: 0,
+                                 bottom: 0
+                             }}/>
+                    </a>
                 </Menu.Item>
-                <Menu.Item key="app" disabled>
-                    <Icon type="appstore" />Navigation Two
+
+                <Menu.Item key="apps">
+                    <a href="/" style={{color:'#ffffff', fontSize:20}}>CSSA APPs</a>
                 </Menu.Item>
-                <SubMenu title={<span><Icon type="setting" />Navigation Three - Submenu</span>}>
-                    <MenuItemGroup title="Item 1">
-                        <Menu.Item key="setting:1">Option 1</Menu.Item>
-                        <Menu.Item key="setting:2">Option 2</Menu.Item>
-                    </MenuItemGroup>
-                    <MenuItemGroup title="Item 2">
-                        <Menu.Item key="setting:3">Option 3</Menu.Item>
-                        <Menu.Item key="setting:4">Option 4</Menu.Item>
-                    </MenuItemGroup>
+
+                <Menu.Item key="logoCSSA" style={{float: 'right', height: 46, width: 70}}>
+                    <a href="http://www.um-cssa.org/" target="__blank">
+                        <img src={logoCSSA}
+                             style={{
+                                 height: 40,
+                                 position: 'absolute',
+                                 margin: 'auto',
+                                 top: 0,
+                                 left: 0,
+                                 right: 0,
+                                 bottom: 0
+                             }}/>
+                    </a>
+                </Menu.Item>
+                <SubMenu title={<span onClick={() => {
+                    window.location.href = "/secondary-market/";
+                }}><Icon type="shopping-cart" />Secondary Market</span>}
+                         style={Object.assign({float: 'right'}, window.location.href.includes('secondary-market') && activeStyle)}>
+                    <Menu.Item key="1">我要购买</Menu.Item>
+                    <Menu.Item key="2">我要出售</Menu.Item>
                 </SubMenu>
-                <Menu.Item key="alipay">
-                    <a href="https://ant.design" target="_blank" rel="noopener noreferrer">Navigation Four - Link</a>
-                </Menu.Item>
+                <SubMenu title={<span onClick={() => {
+                    window.location.href = "/freshman-handbook/";
+                }}><Icon type="book"/>Freshman Handbook</span>}
+                         style={Object.assign({float: 'right'}, window.location.href.includes('freshman-handbook') && activeStyle)}>
+                    <Menu.Item key="1">出国前准备</Menu.Item>
+                    <Menu.Item key="2">入学指南</Menu.Item>
+                    <Menu.Item key="3">超市购物</Menu.Item>
+                    <Menu.Item key="4">休闲娱乐</Menu.Item>
+                    <Menu.Item key="5">交通</Menu.Item>
+                    <Menu.Item key="6">社团介绍</Menu.Item>
+                </SubMenu>
+                <SubMenu title={<span onClick={() => {
+                    window.location.href = "/rate-my-professor/";
+                }}><Icon type="like-o"/>Rate My Professor</span>}
+                         style={Object.assign({float: 'right'}, window.location.href.includes('rate-my-professor') && activeStyle)}>
+                    <Menu.Item key="1"><a href="/rate-my-professor/form/">发布评价</a></Menu.Item>
+                    <Menu.Item key="2"><a href="/rate-my-professor/search/">搜索评价</a></Menu.Item>
+                </SubMenu>
             </Menu>
         );
     }
--- client/src/RateMyProfessorNavbarCollapse.js
+++ client/src/RateMyProfessorNavbarCollapse.js
@@ -54,11 +54,11 @@ export default class RateMyProfessorNavbarCollapse extends React.Component {
                         margin: 0,
                         borderRadius: 0,
                         borderWidth: 0,
-                        backgroundColor: '#0c142d'
+                        backgroundColor: '#001529'
                     }}>
                 <Navbar.Header>
                     <Navbar.Brand>
-                        <a style={{paddingLeft: 50, paddingTop: 0}}><img src={logoM} style={{height: 50}}/></a>
+                        <a style={{paddingLeft: 20, paddingTop: 0}}><img src={logoM} style={{height: 50}}/></a>
                     </Navbar.Brand>
                     <Navbar.Toggle/>
                 </Navbar.Header>
@@ -69,7 +69,7 @@ export default class RateMyProfessorNavbarCollapse extends React.Component {
                         </NavItem>
                     </Nav>
                     <Nav pullRight>
-                        <NavDropdown title="Rate My Professor" id="basic-nav-dropdown-1"
+                        <NavDropdown title="Rate My Professor" id="basic-nav-dropdown-0"
                                      style={this.state.hover[0]? hoverStyle : {}}
                                      onMouseEnter={() => {
                                          this.handleMouseEnter(0);
@@ -80,7 +80,7 @@ export default class RateMyProfessorNavbarCollapse extends React.Component {
                             <MenuItem>发布评价</MenuItem>
                             <MenuItem>搜索评价</MenuItem>
                         </NavDropdown>
-                        <NavDropdown eventKey={3} title="Freshman Handbook" id="basic-nav-dropdown-2"
+                        <NavDropdown eventKey={3} title="Freshman Handbook" id="basic-nav-dropdown-1"
                                      style={this.state.hover[1] ? hoverStyle : {}}
                                      onMouseEnter={() => {
                                          this.handleMouseEnter(1);
@@ -95,16 +95,17 @@ export default class RateMyProfessorNavbarCollapse extends React.Component {
                             <MenuItem>交通</MenuItem>
                             <MenuItem>社团介绍</MenuItem>
                         </NavDropdown>
-                        <NavItem href="#"
-                                 style={this.state.hover[2] ? hoverStyle : {}}
-                                 onMouseEnter={() => {
-                                     this.handleMouseEnter(2);
-                                 }}
-                                 onMouseLeave={() => {
-                                     this.handleMouseLeave(2);
-                                 }}>
-                            Secondary Market
-                        </NavItem>
+                        <NavDropdown title="Secondary Market" id="basic-nav-dropdown-2"
+                                     style={this.state.hover[2]? hoverStyle : {}}
+                                     onMouseEnter={() => {
+                                         this.handleMouseEnter(2);
+                                     }}
+                                     onMouseLeave={() => {
+                                         this.handleMouseLeave(2);
+                                     }}>
+                            <MenuItem>我要购买</MenuItem>
+                            <MenuItem>我要出售</MenuItem>
+                        </NavDropdown>
                         <NavItem href="http://www.um-cssa.org/" target="_blank">
                             <img src={logoCSSA} style={{height: 50, padding: 0}}/>
                         </NavItem>
